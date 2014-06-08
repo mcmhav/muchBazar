73a57108898794f36de808ed15e39093e1e83aa8
Herman Schistad <herman.schistad@gmail.com>
2014-06-08 10:30:07 +0200

    Ignore capitalization

--- src/getTODO.sh
+++ src/getTODO.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-TODOS=$(grep -r "marginpar\|todo\|TODO" masterChapters);
+TODOS=$(grep -ri "marginpar\|todo" masterChapters);
 NUM_TODOS=$(echo "$TODOS" | wc -l | sed -e 's/^ *//');
 echo "$TODOS";
 echo "Found a total of $(tput bold)$NUM_TODOS" todos$(tput sgr0);
